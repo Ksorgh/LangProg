40c26478de0e14138e360d8e1a9ad71d15d4b621
Ksor <90296785+Ksorgh@users.noreply.github.com>
2024-10-28 14:29:24 +0700

    Update to pr002v2.java

--- pr002v2.java
+++ pr002v2.java
@@ -3,34 +3,9 @@ class Help {
     public static void main(String[] args) 
         throws java.io.IOException {
         
-        char choice;
+
         boolean bool = false;
-        System.out.println("Spravocnaya systema po operatoram:");
-        System.out.println("  1. if");
-        System.out.println("  2. Switch");
-        System.out.print("Choose number razdela: ");
-        
-        choice = (char)System.in.read();
-        
-        System.out.println("\n");
-        switch(choice) {
-            case '1':
-                System.out.println("Operator \"IF\":\n");
-                System.out.println("if(yslovie) operator; ili {blok operatorov}");
-                System.out.println("else operator; ili {blok operatorov}");
-                break;
-            case '2':
-                System.out.println("Operator \"Switch\":\n");
-                System.out.println("switch(virazhenie) {");
-                System.out.println("    case \"Const\":");
-                System.out.println("        posledovatelnost operatorov");
-                System.out.println("        break;");
-                System.out.println("        // ...");
-                System.out.println("}");
-                break;      
-            default:
-                System.out.println("Nepravilno ykazan nomer razdela");
-        }
+
 
         if (!bool)
             System.out.println("bool = true");
