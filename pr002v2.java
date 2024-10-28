1d8ccbad59aba5ac7bd3cb79f57fefcbaeffd6a6
Ksor <90296785+Ksorgh@users.noreply.github.com>
2024-10-28 15:10:43 +0700

    Добавили работу с блоками циклов

--- pr002v2.java
+++ pr002v2.java
@@ -1,5 +1,5 @@
 
-class Help {
+class pr002v2 {
     public static void main(String[] args) 
         throws java.io.IOException {
         
@@ -25,25 +25,65 @@ class Help {
         //for(;;);
         int sum=0;
         for(int i=1; i<5; sum += i++);
-        System.out.printl("\nSumma ravna: " + sum);
+        System.out.println("\nSumma ravna: " + sum);
 
         System.out.println("\n");
 
         char ch1='a';
         while(ch1 <= 'z'){
-            System.out.print(ch1 + " ");
+            System.out.println(ch1 + " ");
             ch1++;
         }
-        System.out.println();
-        System.out.println(((int) 'a') + " " + ((int) "z"));
 
 
-        char ch2;
+
+     /*   char ch2;
         do{
             System.out.println("Vvedite simvol i nazmite \"Return\": ");
             ch2 = (char)System.in.read();
-        } while (ch2 != 'q');
-        int result = func1();
+        } while (ch2 != 'q'); */
+
         
+        System.out.println();
+        for(int i=0; i<100; i++){
+            System.out.println("Значение i: " + i);
+            if(i == 5)
+                break;
+            System.out.println("Следующая итерация");
+        }
+
+        System.out.println();
+        for(int i =0; i<3; i++){
+            System.out.println("Счетчик внешнего числа: " + i);
+            System.out.print("Счетчик внутреннего числа: ");
+            int t = 0;
+            while(t<100){
+                if(t==10)
+                    break;
+                System.out.print(t + " ");
+                t++;
+            }
+        }
+        System.out.println();
+        
+        int i2;
+        for(i2=1;i2<4;i2++){
+one:    { 
+two:        {   
+three:          {
+                    System.out.println("\n i2 равно " + i2);
+                    if(i2==1) break one;
+                    if(i2==2) break two;
+                    if(i2==3) break three;
+
+                    System.out.println("Не выполняется");
+                }
+                System.out.println("После блока 3");
+            }
+            System.out.println("После блока 2");
+        }
+        System.out.println("После блока 1");
+        }
+        System.out.println("После цикла for");
     }   
 }
