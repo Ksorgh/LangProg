ec10a45c0a6a4c8360297af0e5c49d166d72589d
Ksor <90296785+Ksorgh@users.noreply.github.com>
2024-10-28 17:31:00 +0700

    Help + OOP | Переделал все под объекты

--- Help.java
+++ Help.java
@@ -1,29 +1,6 @@
-class Help {
-    public static void main(String[] args) 
-    throws java.io.IOException {
-        
-    char choice, ignore;
-    for(;;){
-        do{
-            System.out.println("Spravocnaya systema po operatoram:");
-            System.out.println("  1. if");
-            System.out.println("  2. switch");
-            System.out.println("  3. for");
-            System.out.println("  4. while");
-            System.out.println("  5. do-while");    
-            System.out.println("  6. do-while");      
-            System.out.println("  7. do-while");  
-            System.out.print("Choose number razdela или нажмите \"q\" для выхода ");
-            choice = (char)System.in.read();
-            do{
-                ignore = (char) System.in.read();
-            } while(ignore != '\n');
-        } while(choice < '1' | choice > '7' & choice != 'q');
-        if(choice == 'q') break;
-
-
-        System.out.println("\n");
-        switch(choice) {
+class H{
+    void helpOn(int what){
+        switch(what) {
             case '1':
                 System.out.println("Operator \"IF\":\n");
                 System.out.println("if(yslovie) operator; ili {blok operatorov}");
@@ -63,7 +40,42 @@ class Help {
                 break; 
         }
         System.out.println();
-        }
-         
     }
+    void ShowMenu(){
+        System.out.println("Spravocnaya systema po operatoram:");
+        System.out.println("  1. if");
+        System.out.println("  2. switch");
+        System.out.println("  3. for");
+        System.out.println("  4. while");
+        System.out.println("  5. do-while");    
+        System.out.println("  6. do-while");      
+        System.out.println("  7. do-while");  
+        System.out.print("Choose number razdela или нажмите \"q\" для выхода ");
+    }
+    boolean isValid(int ch){
+        if(ch < '1' | ch > '7' & ch != 'q')
+            return false;
+        else return true;
+    }
+}
+class Help {
+    public static void main(String[] args) 
+    throws java.io.IOException {
+        
+    char choice, ignore;
+        H helpObj = new H();
+        for(;;){
+            do{
+                helpObj.ShowMenu();
+                choice = (char) System.in.read();
+                do{
+                    ignore = (char) System.in.read();
+                }while (ignore != '\n');
+            } while (!helpObj.isValid(choice));
+            if (choice == 'q') break;
+            }
+            System.out.println('\n');
+            helpObj.helpOn(choice);
+
+        } 
 }
