3241f7bb005c360cae4b78f0cdb7d3cc599bcb31
Ksor <90296785+Ksorgh@users.noreply.github.com>
2024-10-28 16:05:08 +0700

    Help v3 | Добавил операторы break и continue

--- Help.java
+++ Help.java
@@ -1,23 +1,25 @@
 class Help {
     public static void main(String[] args) 
-        throws java.io.IOException {
+    throws java.io.IOException {
         
-        char choice, ignore;
-        boolean bool = false;
+    char choice, ignore;
+    for(;;){
         do{
             System.out.println("Spravocnaya systema po operatoram:");
             System.out.println("  1. if");
             System.out.println("  2. switch");
             System.out.println("  3. for");
             System.out.println("  4. while");
-            System.out.println("  5. do-while");        
-            System.out.print("Choose number razdela: ");
+            System.out.println("  5. do-while");    
+            System.out.println("  6. do-while");      
+            System.out.println("  7. do-while");  
+            System.out.print("Choose number razdela или нажмите \"q\" для выхода ");
             choice = (char)System.in.read();
             do{
                 ignore = (char) System.in.read();
             } while(ignore != '\n');
-        } while(choice < '1' | choice > '5');
-        
+        } while(choice < '1' | choice > '7' & choice != 'q');
+        if(choice == 'q') break;
 
 
         System.out.println("\n");
@@ -51,7 +53,17 @@ class Help {
                 System.out.println("operatori");
                 System.out.println("}while(yslovie)");               
                 break;  
+            case '6':
+                System.out.println("Operator \"break\":\n");
+                System.out.println("break; или break метка;");
+                break; 
+            case '7':
+                System.out.println("Operator \"continue\":\n");
+                System.out.println("continue; или continue метка;");
+                break; 
         }
-
+        System.out.println();
+        }
          
-}}
+    }
+}
