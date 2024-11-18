b4462c56b6339373562d8c84fe46777c86cbc3ce
Ksor <90296785+Ksorgh@users.noreply.github.com>
2024-11-18 16:11:29 +0700

    Добавили класс ShowBits и операцию ?
    Добавили класс ShowBits и операцию ?

--- bits.java
+++ bits.java
@@ -1,4 +1,26 @@
 import java.util.Random;
+class ShowBits{
+    int numbits;
+    ShowBits(int n){
+        numbits = n;
+    }
+    void show(long val){
+        long mask = 1;
+        //сдвиг влево значения 1
+        mask <<= numbits-1;
+        int spacer = 0;
+        for(;mask!=0;mask >>>=1){
+            if((val&mask)!=0) System.out.print("1");
+            else System.out.print("0");
+            spacer++;
+            if((spacer % 8) == 0){
+                System.out.print(" ");
+                spacer = 0;
+            }
+        }
+        System.out.println();
+    }
+}
 class bits {
     public static void main(String[] args) {
         Random rand = new Random();
@@ -93,5 +115,39 @@ class bits {
             val2 = val2 >> 1;//сдвиг влево
         }
         System.out.println();
+
+
+        //использование showbits
+        ShowBits b3 = new ShowBits(8);
+        ShowBits i3 = new ShowBits(32);
+        ShowBits li3 = new ShowBits(64);
+
+        System.out.println("Двоичное представление значения 123:");
+        b3.show(123);
+        System.out.println("Двоичное представление значения 12323:");
+        i3.show(12323);
+        System.out.println("Двоичное представление значения 123111111111:");
+        li3.show(123111111111L);
+
+        //операция &
+        //получение модуля числа
+        System.out.println();
+        int absval;
+        val = -32;
+        absval = val < 0 ? - val : val;
+        System.out.println("val и absval: "+ val+" "+absval);
+
+        //предотвращение деления на 0:
+        System.out.println();
+        
+        int res;
+        for(int i = -5; i < 6; i++){
+            res = i != 0 ? 100 / i : 0;
+            if(i != 0)
+                System.out.println("100 / " + i + " равно " + res);
+        }
+
+
+
     }
 }
