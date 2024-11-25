2ee95d6058b2510b1ed2a67441a1a77c0f4063c4
Ksor <90296785+Ksorgh@users.noreply.github.com>
2024-11-25 15:32:23 +0700

    Добавли перегружаемый конструктор для создание очереди на базе другой очереди и массива
    Добавли перегружаемый конструктор для создание очереди на базе другой очереди и массива

--- queue.java
+++ queue.java
@@ -1,10 +1,28 @@
 class Queue{
     private char[] q;
     private int putloc, getloc;
+    //создается пустая очередь заданного размера
     Queue(int size){
         q = new char[size]; 
         putloc = getloc = 0;
     }
+    //создается очередь на основе другого обьекта 
+    Queue(Queue ob){
+        putloc = ob.putloc;
+        getloc = ob.getloc;
+        q = new char[ob.q.length];
+        for(int i=getloc; i<putloc;i++)
+            q[i] = ob.q[i];
+    }
+    //создание очереди на основе массива
+    Queue(char[] a){
+        putloc = 0;
+        getloc = 0;
+        q = new char[a.length];
+        for(int i = 0; i<a.length; i++)
+            put(a[i]);
+    }
+
     void put(char ch){
         if(putloc==q.length){
             System.out.println(" - очередь переполнена");
@@ -55,5 +73,33 @@ class QDemo {
         //попытка доступа к закрытым переменным
         //bigQ.q[3] = 343;
         //bigQ.putloc = 3;
+
+        Queue q1 = new Queue(10);
+        char[] chars = {'A', 'B', 'C'};
+        Queue q2 = new Queue(chars);
+        for(i = 0; i<10; i++)
+            q1.put((char)('D' + i));
+        Queue q3 = new Queue(q1);
+        System.out.print("Очередь q1: " );
+        for(i = 0; i < 10; i++){
+            ch = q1.get();
+            System.out.print(ch);
+        }
+        System.out.println("\n");
+
+        System.out.print("Очередь q2 :" );
+        for(i = 0; i < 10; i++){
+            ch = q2.get();
+            System.out.print(ch);
+        }
+        System.out.println("\n");
+
+        System.out.print("Очередь q3: " );
+        for(i = 0; i < 10; i++){
+            ch = q3.get();
+            System.out.print(ch);
+        }
+        System.out.println("\n");
     }
+    
 }
