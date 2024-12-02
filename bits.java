954ee4af00f9c382bb463b23a83917ea5449e1db
Ksor <90296785+Ksorgh@users.noreply.github.com>
2024-12-02 15:26:42 +0700

    Пример использования внутреннего класса
    Пример использования внутреннего класса

--- bits.java
+++ bits.java
@@ -1,27 +1,37 @@
 import java.util.Random;
-class ShowBits{
-    int numbits;
-    ShowBits(int n){
-        numbits = n;
-    }
-    void show(long val){
-        long mask = 1;
-        //сдвиг влево значения 1
-        mask <<= numbits-1;
-        int spacer = 0;
-        for(;mask!=0;mask >>>=1){
-            if((val&mask)!=0) System.out.print("1");
-            else System.out.print("0");
-            spacer++;
-            if((spacer % 8) == 0){
-                System.out.print(" ");
-                spacer = 0;
+class Bits {
+    public static void main(String[] args) {
+        class ShowBits{
+            int numbits;
+            ShowBits(int n){
+            numbits = n;
+        }
+            void show(long val){
+                long mask = 1;
+                //сдвиг влево значения 1
+                mask <<= numbits-1;
+                int spacer = 0;
+                for(;mask!=0;mask >>>=1){
+                    if((val&mask)!=0) System.out.print("1");
+                    else System.out.print("0");
+                    spacer++;
+                    if((spacer % 8) == 0){
+                        System.out.print(" ");
+                        spacer = 0;
+                    }
+                }
+                System.out.println();
             }
         }
-        System.out.println();
+            for(byte b = -128;b<127;b++){
+                ShowBits byteval = new ShowBits(8);
+                System.out.println(b + " в двоичном виде: ");
+                byteval.show(b);
+            
+        }
     }
 }
-class bits {
+/*class bits2 {
     public static void main(String[] args) {
         Random rand = new Random();
         //сброс 6-го бита, изменение регистра латиницы
@@ -151,3 +161,4 @@ class bits {
 
     }
 }
+*/
