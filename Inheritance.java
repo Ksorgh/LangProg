fd18fc129c31e4155e25b26f2e7b265c26c29e5f
Ksor <90296785+Ksorgh@users.noreply.github.com>
2024-12-09 15:37:03 +0700

    Добавили демонстрацию полиморфизма и класс ColorTriangle
    Добавили демонстрацию полиморфизма и класс ColorTriangle

--- Inheritance.java
+++ Inheritance.java
@@ -34,6 +34,10 @@ class TwoDShape {
     void showDim() {
         System.out.println("Размер фигуры: " + width + " x " + height);
     }
+    double area() {
+        System.out.println("Метод должен быть переопределен!");
+        return 0.0;
+    }
 }
 
 class Triangle extends TwoDShape {
@@ -53,7 +57,17 @@ class Triangle extends TwoDShape {
         return super.name;
     }
 }
-
+class ColorTriangle extends Triangle {
+    String color;
+    ColorTriangle(String c, String s, double w, double h){
+        super(s,w,h); //вызов конструктора Triangle();
+        color = c;
+        name = "Цветной треугольник";
+    }
+    void showColor(){
+        System.out.println("Цвет " + color);
+    }
+}
 class Rectangle extends TwoDShape {
     String outLine;
     Rectangle(String o, double w, double h) {
@@ -76,7 +90,7 @@ class Inheritance {
     public static void main(String[] args) {
         TwoDShape fig1 = new TwoDShape(20.0, 30.0);
         Triangle t1 = new Triangle("Закрашенный", 4.5, 7.0);
-        Triangle t2 = new Triangle("Контурный", 3.5, 8.0);
+        ColorTriangle t2 = new ColorTriangle("Синий","Контурный", 3.5, 8.0);
         Rectangle r1 = new Rectangle("Сплошная линия", 5.0, 5.0); 
         Rectangle r2 = new Rectangle("Пунктирная линия", 5.0, 7.0);
         System.out.println("Информация об обьектах");
@@ -90,11 +104,47 @@ class Inheritance {
         System.out.println("r1: " + r1.name);
         System.out.println("Квадрат: " + r1.isSquare());
         System.out.println("Площадь: " + r1.area());
+
+        //совместимость переменных родственных классов
+        // переменная суперкласса может ссылатся на обьект любого подкласса
+        System.out.println();
+        System.out.println("Ширина двумерной фигуры fig1: " + fig1.getWidth());
+        //при указании на обьект суперкласса выполнятеся метод суперкласса
+        fig1.area();
+        System.out.println("Имя фигуры: " + fig1.name); System.out.println();
+
+        fig1 = t1;
+        System.out.println("Ширина треугольника на который ссылается fig1: " + fig1.getWidth());
+        //при указани на обьект подкласа выполняется вычисление площади для него
+        System.out.println("Площадь треугольника " + fig1.area());
+        System.out.println("Имя фигуры: " + fig1.name); System.out.println();
+
+        fig1 = r1;
+        System.out.println("Ширина прямоугольника на который ссылается fig1: " + fig1.getWidth());
+        //при указани на обьект подкласа выполняется вычисление площади для него
+        System.out.println("Площадь прямоугольника " + fig1.area());
+        System.out.println("Имя фигуры: " + fig1.name); System.out.println();
+
+        fig1 = t2;
+        t2.showColor();
+        System.out.println("Площадь цветного треугольника " + fig1.area());
+        System.out.println("Имя фигуры: " + fig1.name);
+
+        //доступ к членам мподкласса для ссылочных переменных закрыт 
+        //System.out.println(fig1.style);
+
         //проверка доступности переменных
         /* 
         System.out.println(fig1.width);
         System.out.println(t1.width); //подкласс имеет доступ к переменным и методам суперкласса
         System.out.println(fig1.style);//супер класс не имеет доступа к переменным описаных в подклассе
         */   
+        /* 
+        float n1 = 0x7F7FFFFF;
+        int i1 = (int)n1;
+        System.out.println(i1);
+        System.out.println((int)n1);
+        */
+
     }
 }
